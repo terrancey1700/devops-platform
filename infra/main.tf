8ba1dd1c6b18063d781a9a5d1b24acadebb8d96d
Terrance Young <terrance@empowered901.com>
2026-07-06 15:29:24 -0500

    feat: add public subnet

---
+++ infra/main.tf
@@ -0,0 +1,20 @@
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "${var.environment}-vpc"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "${var.aws_region}a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.environment}-public-subnet"
+  }
+}
