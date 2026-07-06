5d91cd79e0b24b668cf798eba1e9c7d6c9532a86
Terrance Young <terrance@empowered901.com>
2026-07-06 16:14:14 -0500

    feat: add public routing

--- infra/main.tf
+++ infra/main.tf
@@ -18,3 +18,29 @@ resource "aws_subnet" "public" {
     Name = "${var.environment}-public-subnet"
   }
 }
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.environment}-igw"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "${var.environment}-public-rt"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
